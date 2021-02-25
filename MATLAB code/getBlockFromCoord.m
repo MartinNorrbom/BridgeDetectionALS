abf4ef9cf48d65ed524f3d15d47a4125d9308995
MartinNorrbom <snaffe.kaffe@gmail.com>
2021-02-25 13:59:44 +0100

    Fixed comments

--- MATLAB code/getBlockFromCoord.m
+++ MATLAB code/getBlockFromCoord.m
@@ -1,9 +1,30 @@
 function [dataSetSkog,returnNumberBlock,intensityBlock,pointLabel,blockLabel] = ...
     getBlockFromCoord(ptCloud,pointAttributes,class,tileBlockPointNumber,gridSize, coordinates)
-%getBlockFromCoord 
+%getBlockFromCoord generates tile blocks from selected coordinates. It set
+% the current coordinate in the list to the middle of the tile block, then
+% if a coordinate was within a tile block that was generated earlier it
+% skip to generate another tile block for this coordinate.
 
+%   Input: 
+%       ptCloud: point cloud data
+%       pointAttributes: the attributes of point cloud
+%       class: the class of points
+%       tileBlockPointNumber: number of points in unit tile block
+%       gridSize: the size of time block (m x m)
+%       coordinates: the list of coordinates where tile blocks are going to
+%                   be generated.
+%   Output:
+%       dataSetSkog: XYZ coordinates (the location) of points in tile block
+%       intensityBlock: the intensity of the points in tile block
+%       returnNumberBlock: return numbers of the points in tile block
+%       pointLabel: Label for each point in the tile blocks. 0 is non
+%                   bridge 1 is bridge.
+%       blockLabel: Label for each tile blocks, 0 is non bridge 1 is bridge.
+
+    % Maximum number of tile blocks.
     numberOfBlock = size(coordinates,1);
 
+    % Create a list to indicate if each coordinate has a tile block.
     coordinateCheckList = false(numberOfBlock,1);
     
     % Allocate space to return tile block data.
@@ -13,17 +34,23 @@ function [dataSetSkog,returnNumberBlock,intensityBlock,pointLabel,blockLabel] = 
     pointLabel = single(zeros([tileBlockPointNumber numberOfBlock]));
     blockLabel = single(ones([1 numberOfBlock]));
 
+    % Loops until all tile blocks are generated.
     ii=0;
     coordCount = 0;
     while sum(coordinateCheckList) < numberOfBlock
+        
+        % Count cordinate list.
         coordCount = coordCount+1;
         if(coordinateCheckList(coordCount) == false)
+            % Count the number of generated tile blocks.
             ii = ii+1;
 
-            xLim = coordinates(coordCount,1)+[1,-1]*gridSize; %+-50m in x axis from the current point
-            yLim = coordinates(coordCount,2)+[1,-1]*gridSize; %+-50m in y axis from the current point
+            % Get the limits of the tile block, with the selected
+            % coordinate in the middle.
+            xLim = coordinates(coordCount,1)+[1,-1]*gridSize; 
+            yLim = coordinates(coordCount,2)+[1,-1]*gridSize; 
 
-            % Select the coordinates that lie inside the tile block
+            % Select the coordinates that lie inside the tile block.
             coordInBlockLimit = ...
             (coordinates(:,1)<=xLim(1)) & (coordinates(:,1)>xLim(2))& ...
             (coordinates(:,2)<=yLim(1)) & (coordinates(:,2)>yLim(2));
@@ -36,7 +63,7 @@ function [dataSetSkog,returnNumberBlock,intensityBlock,pointLabel,blockLabel] = 
             (ptCloud.Location(:,1)<=xLim(1)) & (ptCloud.Location(:,1)>xLim(2))& ...
             (ptCloud.Location(:,2)<=yLim(1)) & (ptCloud.Location(:,2)>yLim(2));
 
-
+            % Get the number of points in the tile block.
             numberOfPointsInBlock = sum(pointsInBlockLimit);
 
             if(numberOfPointsInBlock< tileBlockPointNumber)      
